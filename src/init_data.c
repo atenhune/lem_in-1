28877a095388dbe6eea29f5f24f37e2383ec8425
Aleksandr Tikka <altikka@student.hive.fi>
2022-09-15 17:37:32 +0300

    initializes data to be parsed onto

---
+++ src/init_data.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_data.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/15 15:56:51 by altikka           #+#    #+#             */
+/*   Updated: 2022/09/15 17:28:41 by altikka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem_in.h"
+
+int	init_data(t_lem *d)
+{
+	ft_bzero(d, sizeof(*d));
+	if(ft_vecnew(&d->rooms, 2, sizeof(t_room)) < 0)
+		return (panic(NULL, "Error: Couldn't initialize rooms."));
+	return (1);
+}
