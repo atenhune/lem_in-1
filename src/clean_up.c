57521ead71d22949faacbb402605af9e496eee0d
Aleksandr Tikka <altikka@student.hive.fi>
2022-09-15 17:39:19 +0300

    has panic, free_data and free_room_data functions
    panic prints msg to stdout 2. free_data frees struct data with the help of free_room_data

---
+++ src/clean_up.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clean_up.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/15 16:25:45 by altikka           #+#    #+#             */
+/*   Updated: 2022/09/15 17:30:40 by altikka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem_in.h"
+
+static void	free_room_data(t_room *room)
+{
+	ft_strdel(&room->name);
+	ft_vecdel(&room->links);
+}
+
+void	free_data(t_lem *d)
+{
+	t_room	*temp;
+	size_t	i;
+
+	if (d->rooms.data)
+	{
+		i = 0;
+		while (i < d->rooms.len)
+		{
+			temp = ft_vecget(&d->rooms, i);
+			free_room_data(temp);
+			i++;
+		}
+		temp = NULL;
+		ft_vecdel(&d->rooms);
+	}
+}
+
+int	panic(t_lem *d, const char *msg)
+{
+	ft_putendl_fd(msg, 2);
+	if (d)
+		free_data(d);
+	return (-1);
+}
