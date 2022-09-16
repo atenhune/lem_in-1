fe35121635434788292fc42bedd277425a7ad5da
Aleksandr Tikka <altikka@student.hive.fi>
2022-09-16 19:43:33 +0300

    defines and functions for the parser
    one enum and one struct 't_parse' and also three functions for parsing ant, room and link data

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 11:09:22 by altikka           #+#    #+#             */
-/*   Updated: 2022/09/16 11:03:51 by altikka          ###   ########.fr       */
+/*   Updated: 2022/09/16 19:41:32 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,10 @@
 # define LEM_IN_H
 
 # include "libft.h"
+
+/*
+**STRUCTS FOR THE PROGRAM
+*/
 
 typedef struct s_room
 {
@@ -32,6 +36,42 @@ typedef struct s_lem
 	int		start;
 	int		end;
 }			t_lem;
+
+/*
+**STRUCTS & FUNCTIONS FOR THE PARSER
+*/
+
+typedef enum e_state
+{
+	ANTS,
+	ROOMS,
+	LINKS,
+	DONE
+}			t_state;
+
+typedef struct s_parse
+{
+	char	*line;
+	t_state	state;
+	int		start;
+	int		end;
+}			t_parse;
+
+int		number_of_ants(t_lem *d, t_parse *p);
+int		the_rooms(t_lem *d, t_parse *p);
+int		the_links(t_lem *d, t_parse *p);
+
+typedef int				(*t_parsers)(t_lem *, t_parse *);
+
+static const t_parsers	g_parsers[3] = {
+	number_of_ants,
+	the_rooms,
+	the_links
+};
+
+/*
+**FUNCTIONS FOR THE PROGRAM
+*/
 
 int		init_data(t_lem *d);
 int		panic(t_lem *d, const char *msg);
