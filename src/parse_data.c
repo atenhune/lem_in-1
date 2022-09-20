6dc8a61e3dd70425f02dabb655587b53f6fabc41
Aleksandr Tikka <altikka@student.hive.fi>
2022-09-20 18:32:50 +0300

    added free_parser & free_parser_data functions and more...
    collect_instructions saves all read lines to s_parser. get_commands seeks for start & end and flags them. number_of_ants has the most basic functionality now

--- src/parse_data.c
+++ src/parse_data.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 09:35:18 by altikka           #+#    #+#             */
-/*   Updated: 2022/09/19 19:05:26 by altikka          ###   ########.fr       */
+/*   Updated: 2022/09/20 18:26:28 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 ** iii: links
 */
 
-int	the_links(t_lem *d, t_parse *p)
+int	the_links(t_lem *d, t_parser *p)
 {
 	(void )d;
 	p->state = DONE;
@@ -27,35 +27,77 @@ int	the_links(t_lem *d, t_parse *p)
 	return (1);
 }
 
-int	the_rooms(t_lem *d, t_parse *p)
+int	the_rooms(t_lem *d, t_parser *p)
 {
 	(void )d;
+	//first check whether ' ' -splitted arr has something in [1]
+	//if not, we go to LINKS;
 	p->state = LINKS;
 	ft_printf("got rooms, next->links\n");
 	return (1);
-
 }
 
 static int	ft_isnbr(const char *str)
 {
-	(void )str;
+	while (*str)
+	{
+		if (!ft_isdigit(*str))
+			return (0);
+		str++;
+	}
 	return (1);
 }
 
-int	number_of_ants(t_lem *d, t_parse *p)
+int	number_of_ants(t_lem *d, t_parser *p)
 {
-	(void )d;
+	if (!ft_isnbr(p->line))
+		return (-1);
+	d->ants = ft_atoi(p->line);
+	if (d->ants < 0)
+		return (-1);//message: "You monster."
+	if (d->ants == 0)
+		return (-1);//message: "No ants".
 	p->state = ROOMS;
-	ft_isnbr(NULL);
 	ft_printf("got ants, next->rooms\n");
 	return (1);
 }
 
-static int	get_command(t_parse *p)
+static int	collect_instructions(t_parser *p, const char c)
 {
-	(void )p;
+	if (ft_vecncat(&p->inputs, p->line, ft_strlen(p->line)) < 0)
+		return (-1);
+	if (ft_vecpush(&p->inputs, &c) < 0)
+		return (-1);
+	ft_strdel(&p->line);
 	return (1);
 }
+
+static int	get_commands(t_parser *p)
+{
+	if (!ft_strncmp(p->line, "##start", 8))
+		p->start++;
+	else if (!ft_strncmp(p->line, "##end", 6))
+		p->end++;
+	return (1);
+}
+//FREE
+////////////////////////////////////////////////////
+static void	free_parser_data(t_parser *p)
+{
+	if (p->inputs.data)
+		ft_vecdel(&p->inputs);
+	if (p->line)
+		ft_strdel(&p->line);
+}
+
+static int	free_parser(t_parser *p, const char *msg)
+{
+	ft_putendl_fd(msg, 2);
+	if (p)
+		free_parser_data(p);
+	return (-1);
+}
+////////////////////////////////////////////////////
 
 static int	return_next_line(const int fd, char **line, int *ret)
 {
@@ -63,32 +105,43 @@ static int	return_next_line(const int fd, char **line, int *ret)
 	return (*ret);
 }
 
-static void	init_parser(t_parse *p)
+static int	init_parser(t_parser *p)
 {
+	ft_bzero(p, sizeof(*p));
+	if (ft_vecnew(&p->inputs, 1, sizeof(char)) < 0)//'1' could be bigger
+		return (-1);
 	p->line = NULL;
 	p->state = ANTS;
 	p->start = 0;
 	p->end = 0;
+	return (1);
 }
 
-int	parse_data(t_lem *d)
+int	parse_data(t_lem *d, t_vec *farm)
 {
-	t_parse	p;
-	int		ret;
+	t_parser	p;
+	int			ret;
 
-	init_parser(&p);
-	while (return_next_line(0, &p.line, &ret) && p.state != DONE)
+	(void)farm;
+	if (init_parser(&p) < 0)
+		return (panic(NULL, "Error: Initializing parser failed."));
+	while (return_next_line(0, &p.line, &ret) && p.state != DONE)//remove DONE later
 	{
 		if (ret == -1)
-			return (panic(d, "Error: GNL exited with -1."));
+			return (free_parser(&p, "Error: GNL error."));
 		if (p.line[0] == '#')
 		{
-			if (get_command(&p) < 0)
-				return (panic(d, "Error: Invalid command."));
+			if (get_commands(&p) < 0)
+				return (free_parser(&p, "Error: Invalid command."));
 		}
 		else if (g_parsers[p.state](d, &p) < 0)
-			return (panic(d, "Error: Invalid ants/rooms/links."));
+			return (free_parser(&p, "Error: Invalid ants/rooms/links."));
+		if (collect_instructions(&p, '\n') < 0)
+			return (free_parser(&p, "Error: Memory issue."));
 	}
 	ft_printf("DONE\n");
+	ft_printf("GOT %d ANTS\n", d->ants);
+	write(1, p.inputs.data, p.inputs.len);
+	free_parser_data(&p);
 	return (1);
 }
