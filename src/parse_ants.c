1095372f33f0c4ad1ea163414edf9328bf7d00a0
Aleksandr Tikka <altikka@student.hive.fi>
2022-09-21 16:18:27 +0300

    own function now - saves ant ammount to struct

---
+++ src/parse_ants.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_ants.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/21 11:06:32 by altikka           #+#    #+#             */
+/*   Updated: 2022/09/21 11:08:10 by altikka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem_in.h"
+
+int	parse_ants(t_lem *d, t_parser *p)
+{
+	if (!ft_isnbr(p->line))
+		return (-1);
+	d->ants = ft_atoi(p->line);
+	if (d->ants < 0)
+		return (panic(NULL, "Error: Negative ammount of ants."));
+	if (d->ants == 0)
+		return (panic(NULL, "Error: No ants."));
+	p->state = ROOMS;
+	ft_printf("got ants, next->rooms\n");//del
+	return (1);
+}
