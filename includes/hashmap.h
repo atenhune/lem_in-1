f82c8e8cc9d25df98d2139d653dd3566bc868644
home <aleksandr.tikka@gmail.com>
2022-09-23 15:30:38 +0300

    atenhune's hash structure & function to be added later to the program

---
+++ includes/hashmap.h
@@ -0,0 +1,21 @@
+//header
+
+#ifndef HASHMAP_H
+
+# define HASHMAP_H
+
+# include "libft.h"
+
+# define HASH_TABLE_SIZE 10000
+
+typedef struct s_hash
+{
+	char	*room;
+}		t_hash;
+
+
+unsigned long	hash(char *name);
+int				hash_table_insert(char *room, t_hash *hash_table);
+int				hash_lookup(char *room, t_hash *hash_table);
+
+#endif
