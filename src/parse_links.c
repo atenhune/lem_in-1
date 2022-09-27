f61e2d177622112b0e9a53f0441937feabf4b240
Aleksandr Tikka <altikka@student.hive.fi>
2022-09-27 20:47:27 +0300

    parsing link data all here

--- src/parse_links.c
+++ src/parse_links.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 11:14:18 by altikka           #+#    #+#             */
-/*   Updated: 2022/09/21 11:15:02 by altikka          ###   ########.fr       */
+/*   Updated: 2022/09/27 20:46:19 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,23 @@
 
 int	parse_links(t_lem *d, t_parser *p)
 {
-	(void )d;
-	p->state = DONE;
+	char	*ptr;
+	int		target;
+	int		to;
+	t_room	*temp;
+
+	ptr = ft_strchr(p->line, '-');
+	*ptr = '\0';
+	target = hash_get(p->line, *(&p->table));
+	if (target == -1)
+		return (panic(NULL, "Error: Invalid link (1)."));
+	*ptr++ = '-';
+	to = hash_get(ptr, *(&p->table));
+	if (to== -1)
+		return (panic(NULL, "Error: Invalid link (2)."));
+	temp = ft_vecget(&d->rooms, target);
+	ft_vecpush(&temp->links, &to);
+	//p->state = DONE;
 	ft_printf("got links, DONE\n");//del
 	return (1);
 }
