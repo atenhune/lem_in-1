573be9eb93b6d122078b6fac359ff44ae361ffdd
Aleksandr Tikka <altikka@student.hive.fi>
2022-09-27 20:32:18 +0300

    init start and end to -1

--- src/init_data.c
+++ src/init_data.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 15:56:51 by altikka           #+#    #+#             */
-/*   Updated: 2022/09/15 21:34:36 by altikka          ###   ########.fr       */
+/*   Updated: 2022/09/27 16:33:53 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,7 @@ int	init_data(t_lem *d)
 	ft_bzero(d, sizeof(*d));
 	if (ft_vecnew(&d->rooms, 2, sizeof(t_room)) < 0)
 		return (panic(NULL, "Error: Couldn't initialize rooms."));
+	d->start = -1;
+	d->end = -1;
 	return (1);
 }
