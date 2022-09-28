a3ffb77c290ccf39d28a81be4b3c522d694dad3f
home <aleksandr.tikka@gmail.com>
2022-09-28 10:05:17 +0300

    better debugging print

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 11:07:37 by altikka           #+#    #+#             */
-/*   Updated: 2022/09/27 20:19:00 by altikka          ###   ########.fr       */
+/*   Updated: 2022/09/28 09:58:43 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,47 +21,35 @@
 ** iv: print
 */
 
-static void	test_print(t_lem *d)
+static void	test_print(t_lem *d, t_vec *farm)
 {
-	t_room	*temp;
-	//int		*n;
+	t_room	*room;
+	t_room	*link;
+	int		*n;
 	size_t	i;
+	size_t	j;
 
-	ft_printf("start: %d, end: %d\n", d->start, d->end);
+	write(1, farm->data, farm->len);
+	ft_printf("\n\ngot %d ants.\n\n", d->ants);
 	i = 0;
 	while (i < d->rooms.len)
 	{
-		temp = ft_vecget(&d->rooms, i);
-		//n = ft_vecget(&temp->links, 0);
-		//ft_printf("name: %s | link: %d\n", temp->name, *n);
-		ft_printf("room [%d], name: %s, index: |%d|\n", (i), temp->name, temp->index);
+		room = ft_vecget(&d->rooms, i);
+		ft_printf("room: %s,\tindex: [%d],\tlinks: ", room->name, room->index);
+		j = 0;
+		while (j < room->links.len)
+		{
+			n = ft_vecget(&room->links, j);
+			link = ft_vecget(&d->rooms, *n);
+			ft_printf("%s ", link->name);
+			j++;
+		}
 		i++;
+		ft_printf("\n");
 	}
 	ft_printf("\n");
 }
-/*
-static void	test_parse(t_lem *d)
-{
-	t_room	one;
-	t_room	two;
-	int		a;
-	int		b;
 
-	one.name = ft_strnew(5);
-	two.name = ft_strnew(5);
-	ft_strcpy(one.name, "hello");
-	ft_strcpy(two.name, "world");
-	ft_vecnew(&one.links, 1, sizeof(int));
-	ft_vecnew(&two.links, 1, sizeof(int));
-	a = 1000;
-	b = 44;
-	ft_vecpush(&one.links, &a);
-	ft_vecpush(&two.links, &b);
-	ft_vecpush(&d->rooms, &one);
-	ft_vecpush(&d->rooms, &two);
-	test_print(d);
-}
-*/
 int	main(int argc, char **argv)
 {
 	t_lem	d;
@@ -70,14 +58,14 @@ int	main(int argc, char **argv)
 	//* +: -q etc
 	(void ) argc;
 	(void ) argv;
-	if (init_data(&d) < 0)
+	if (init_data(&d, &farm) < 0)
 		return (panic(NULL, "Error: Initializing data failed."));
-	//test_parse(&d);
 	if (parse_data(&d, &farm) < 0)
 		return (panic(&d, "Error: Parsing data failed."));
-	//* iii: solve
-	//* iv: print
-	test_print(&d);
+	//* iii: solve()
+	//* iv: print()
+	test_print(&d, &farm); //debugging
+	ft_vecdel(&farm); //will be freed in print()
 	free_data(&d);
 	system("leaks -q lem-in");
 	return (0);
