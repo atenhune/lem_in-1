86d347054333680370d1ca11e969f406e7653377
home <aleksandr.tikka@gmail.com>
2022-09-28 10:04:17 +0300

    initializes t_vec farm for saving data

--- src/init_data.c
+++ src/init_data.c
@@ -6,17 +6,19 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 15:56:51 by altikka           #+#    #+#             */
-/*   Updated: 2022/09/27 16:33:53 by altikka          ###   ########.fr       */
+/*   Updated: 2022/09/28 09:33:17 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-int	init_data(t_lem *d)
+int	init_data(t_lem *d, t_vec *farm)
 {
 	ft_bzero(d, sizeof(*d));
 	if (ft_vecnew(&d->rooms, 2, sizeof(t_room)) < 0)
 		return (panic(NULL, "Error: Couldn't initialize rooms."));
+	if (ft_vecnew(farm, 32, sizeof(char)) < 0) //32 min chars in map
+		return (-1); //what
 	d->start = -1;
 	d->end = -1;
 	return (1);
