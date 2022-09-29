662d97c17097cc64445ee2bfb5825713535430e1
Aleksandr Tikka <altikka@student.hive.fi>
2022-09-29 17:14:06 +0300

    moved init_parser() here for clarity

--- src/init_data.c
+++ src/init_data.c
@@ -6,11 +6,37 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 15:56:51 by altikka           #+#    #+#             */
-/*   Updated: 2022/09/28 09:33:17 by altikka          ###   ########.fr       */
+/*   Updated: 2022/09/29 15:43:24 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
+
+static void	init_hash_table(t_parser *p)
+{
+	int	i;
+
+	i = 0;
+	while (i < TABLE_SIZE)
+	{
+		p->table[i].room = NULL;
+		p->table[i].index = -1;
+		i++;
+	}
+}
+
+int	init_parser(t_parser *p)
+{
+	ft_bzero(p, sizeof(*p));
+	if (ft_vecnew(&p->inputs, 32, sizeof(char)) < 0) //32 min chars in map
+		return (-1); //what
+	p->line = NULL;
+	p->start = 0;
+	p->end = 0;
+	p->state = ANTS;
+	init_hash_table(p);
+	return (1);
+}
 
 int	init_data(t_lem *d, t_vec *farm)
 {
@@ -19,6 +45,7 @@ int	init_data(t_lem *d, t_vec *farm)
 		return (panic(NULL, "Error: Couldn't initialize rooms."));
 	if (ft_vecnew(farm, 32, sizeof(char)) < 0) //32 min chars in map
 		return (-1); //what
+	d->links = NULL; //I need freeing y'know
 	d->start = -1;
 	d->end = -1;
 	return (1);
