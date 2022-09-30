0bbbd25a595ca5739306d19f12d40e569d5e1faf
Antti-pekka Tenhunen <atenhune@c2r2p2.hive.fi>
2022-09-30 19:50:27 +0300

    debugging comments

--- src/solve.c
+++ src/solve.c
@@ -6,7 +6,7 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 10:55:18 by atenhune          #+#    #+#             */
-/*   Updated: 2022/09/30 12:26:38 by atenhune         ###   ########.fr       */
+/*   Updated: 2022/09/30 19:35:41 by atenhune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,21 @@
 static void	zero_bfs(t_lem *d, t_bfs *bf)
 {
 	int	i;
-	int	j;
 
+	ft_memset(bf->fl_dir, -1, sizeof(int) * d->room_count);
 	i = 0;
 	while (i < d->room_count)
-	{
-		j = 0;
-		bf->fl_dir[i] = -1;
-		while (j < d->room_count)
-			bf->flow[i][j++] = 0;
-		i++;
-	}
+		ft_memset(bf->flow[i++], 0, sizeof(int) * d->room_count);
 }
 
 static void	init_bfs(t_lem *d, t_bfs *bfs)
 {
 	int		i;
 
-	i  = 0;
 	bfs->best = NULL;
 	bfs->fl_dir = malloc(sizeof(int) * d->room_count);
 	bfs->flow = (int **)malloc(sizeof(int *) * d->room_count);
+	i  = 0;
 	while (i < d->room_count)
 		bfs->flow[i++] = malloc(sizeof(int) * d->room_count);
 	zero_bfs(d, bfs);
@@ -45,16 +39,21 @@ int solve(t_lem *d)
 {
 	t_bfs		bf;
 	int			temp;
-	t_room 		*room;
 
 	init_bfs(d, &bf);
 	temp = 0;
+	// while (temp < 53)
+	// {
+	// 	bfs(d, &bf);
+	// 	temp++;
+	// }
 	while (bfs(d, &bf))
 		temp++;
 	// double_check_paths(d, bf);
 	// printf("\nSECOND BFS:\n");
 	size_t	i = 0;
 	size_t	j = 0;
+	t_room 		*room;
 	while (bf.best->paths[i][j] != -1)
 	{
 		while (bf.best->paths[i][j] != -1)
@@ -62,12 +61,12 @@ int solve(t_lem *d)
 			room = ft_vecget(&d->rooms, bf.best->paths[i][j]);
 			printf("|%s| ", room->name);
 			j++;
-			if (bf.best->paths[i][j] != -1)
-				printf("-> ");
+			// if (bf.best->paths[i][j] != -1)
+				// printf("-> ");
 		}
 		j = 0;
 		i++;
-		printf("\n-------------------\n");
+		// printf("\n-------------------\n");
 	}
 	// debug_bfs(d, bf, set);
 	printf(">>>> %d <<<<\n", bf.best->turns);
