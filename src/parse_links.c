0b4bd462a43485ca7129d2abffe90c028c38930e
home <aleksandr.tikka@gmail.com>
2022-09-30 23:03:03 +0300

    cleaning up & better error handling

--- src/parse_links.c
+++ src/parse_links.c
@@ -6,7 +6,7 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 11:14:18 by altikka           #+#    #+#             */
-/*   Updated: 2022/09/30 12:49:02 by atenhune         ###   ########.fr       */
+/*   Updated: 2022/09/30 22:58:55 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,15 +25,16 @@ int	parse_links(t_lem *d, t_parser *p)
 	*ptr = '\0';
 	from = hash_get(p->line, *(&p->table));
 	if (from == -1)
-		return (panic(NULL, "Error: Invalid link (1)."));
+		return (panic(NULL, "Error: Invalid 1st link."));
 	*ptr++ = '-';
 	to = hash_get(ptr, *(&p->table));
 	if (to == -1)
-		return (panic(NULL, "Error: Invalid link (2)."));
+		return (panic(NULL, "Error: Invalid 2nd link."));
 	temp = ft_vecget(&d->rooms, from);
-	ft_vecpush(&temp->links, &to);
+	if (ft_vecpush(&temp->links, &to) < 0)
+		return (panic(NULL, "Error: Couldn't save 1st room's link."));
 	temp = ft_vecget(&d->rooms, to);
-	ft_vecpush(&temp->links, &from);
-	//p->state = DONE;
+	if (ft_vecpush(&temp->links, &from) < 0)
+		return (panic(NULL, "Error: Couldn't save 2nd room's link."));
 	return (1);
 }
