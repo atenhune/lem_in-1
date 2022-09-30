d2e39fe305f10f82dd241e90a5cb42db116f70f0
home <aleksandr.tikka@gmail.com>
2022-09-30 23:02:33 +0300

    cleaning up & better error handling

--- src/init_data.c
+++ src/init_data.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 15:56:51 by altikka           #+#    #+#             */
-/*   Updated: 2022/09/29 15:43:24 by altikka          ###   ########.fr       */
+/*   Updated: 2022/09/30 22:49:17 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@ static void	init_hash_table(t_parser *p)
 int	init_parser(t_parser *p)
 {
 	ft_bzero(p, sizeof(*p));
-	if (ft_vecnew(&p->inputs, 32, sizeof(char)) < 0) //32 min chars in map
-		return (-1); //what
+	if (ft_vecnew(&p->inputs, 32, sizeof(char)) < 0)
+		return (panic(NULL, "Error: Couldn't initialize inputs."));
 	p->line = NULL;
 	p->start = 0;
 	p->end = 0;
@@ -43,9 +43,9 @@ int	init_data(t_lem *d, t_vec *farm)
 	ft_bzero(d, sizeof(*d));
 	if (ft_vecnew(&d->rooms, 2, sizeof(t_room)) < 0)
 		return (panic(NULL, "Error: Couldn't initialize rooms."));
-	if (ft_vecnew(farm, 32, sizeof(char)) < 0) //32 min chars in map
-		return (-1); //what
-	d->links = NULL; //I need freeing y'know
+	if (ft_vecnew(farm, 32, sizeof(char)) < 0)
+		return (panic(NULL, "Error: Couldn't initialize farm."));
+	d->links = NULL;
 	d->start = -1;
 	d->end = -1;
 	return (1);
