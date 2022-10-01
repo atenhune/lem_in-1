ad60f69da37505c1e365621bcb6ec228f935b6bc
home <aleksandr.tikka@gmail.com>
2022-10-01 13:19:57 +0300

    v1.1
    prints report and has option to remove all trace maps

--- run.sh
+++ run.sh
@@ -9,13 +9,17 @@ EOC=$(tput sgr0)
 if [ "$1" == "" ] || [ "$#" -ne 1 ] || [[ ! $1 =~ ^-?[[:digit:]]+$ ]]
 then
 	printf "\nusage: ./run.sh [number]\n
-	e.g. '${YELLOW}./run.sh 50${EOC}' will run the script 50 times on big-superposition maps.
-	selected maps will be saved in maps/trace_maps/ directory.
-	results better than expected will be shown in ${GREEN}green${EOC},
-	results worse than will be highlighted in ${RED}red${EOC}.\n\n"
+	E.g. '${YELLOW}./run.sh 50${EOC}' will run the script 50 times on big-superposition maps.
+	Selected maps will be saved in maps/trace_maps/ directory.
+	Results better than expected will be shown in ${GREEN}green${EOC},
+	and results worse than will be highlighted in ${RED}red${EOC}.\n\n"
 	exit
 fi
 
+declare -i UNDER=0
+declare -i U_RES=0
+declare -i OVER=0
+declare -i O_RES=0
 declare -i I=0
 
 printf "\nTesting $1 big-superposition maps.\n\n"
@@ -34,22 +38,50 @@ do
 	then
 		COLOR=${GREEN}
 		DIFF=$(($EXPECTED - $RESULT))
+		UNDER=$((UNDER + 1))
+		U_RES=$((U_RES + DIFF))
 		if [ $DIFF -gt 2 ]
 		then
 			cp maps/trace_maps/temp.map maps/trace_maps/under_${I}__${DIFF}.map
-			MSG="\t-${DIFF} -->\tunder_${I}__${DIFF}.map"
+			MSG="\t+${DIFF} -->\tunder_${I}__${DIFF}.map"
 		fi
 	elif [ $EXPECTED -lt $RESULT ]
 	then
 		COLOR=${RED}
 		DIFF=$(($RESULT - $EXPECTED))
+		OVER=$((OVER + 1))
+		O_RES=$((O_RES + DIFF))
 		cp maps/trace_maps/temp.map maps/trace_maps/trace_${I}__${DIFF}.map
-		MSG="\t+${DIFF} -->\ttrace_${I}__${DIFF}.map"
+		MSG="\t-${DIFF} -->\ttrace_${I}__${DIFF}.map"
 	fi
 
 	printf "Expected: $EXPECTED | Result: ${COLOR}$RESULT${EOC}${MSG}\n"
 
 	rm -fr maps/trace_maps/temp.map
 	I=$((I + 1))
-	sleep 1
+	sleep 0.3
 done
+
+printf "DONE\n\nReport:\n"
+printf " ${GREEN}${UNDER}${EOC}/$1 of maps were solved better than expected.\n"
+if [ $UNDER -gt 0 ]
+then
+	U_AVR=$(echo "$U_RES/$UNDER" | bc -l)
+	printf "  avarage: +"
+	printf %.1f\\n $U_AVR
+fi
+
+printf " ${RED}${OVER}${EOC}/$1 of maps were solved worse than expected.\n"
+if [ $OVER -gt 0 ]
+then
+	O_AVR=$(echo "$O_RES/$OVER" | bc -l)
+	printf "  avarage: -"
+	printf %.1f\\n\\n $O_AVR
+fi
+printf "\nDo you wish to remove trace maps? (yes/no)\n"
+read remove_trace
+if [ "$remove_trace" == "yes" ]
+then
+	rm -fr maps/trace_maps/
+fi
+exit
