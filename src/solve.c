bf286301b87652122e86bfbefecb978baf6b0421
Antti-pekka Tenhunen <atenhune@c2r2p2.hive.fi>
2022-10-03 20:33:09 +0300

    removed init_bfs() and zero_bfs()

--- src/solve.c
+++ src/solve.c
@@ -6,74 +6,34 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 10:55:18 by atenhune          #+#    #+#             */
-/*   Updated: 2022/10/01 17:59:59 by atenhune         ###   ########.fr       */
+/*   Updated: 2022/10/03 15:03:57 by atenhune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-static void	zero_bfs(t_lem *d, t_bfs *bf)
+// static void	sort_paths(t_lem *d, t_bfs *bf)
+// {
+// 	size_t	i;
+
+// 	i = 0;
+	
+// }
+
+int	solve(t_lem *d, t_bfs *bf)
 {
-	int	i;
+	int	ret;
 
-	ft_memset(bf->fl_dir, -1, sizeof(int) * d->room_count);
-	i = 0;
-	while (i < d->room_count)
-		ft_memset(bf->flow[i++], 0, sizeof(int) * d->room_count);
-}
-
-static void	init_bfs(t_lem *d, t_bfs *bfs)
-{
-	int		i;
-
-	bfs->best = NULL;
-	bfs->fl_dir = malloc(sizeof(int) * d->room_count);
-	bfs->flow = (int **)malloc(sizeof(int *) * d->room_count);
-	i  = 0;
-	while (i < d->room_count)
-		bfs->flow[i++] = malloc(sizeof(int) * d->room_count);
-	zero_bfs(d, bfs);
-}
-
-int solve(t_lem *d)
-{
-	t_bfs		bf;
-	int			temp;
-
-	init_bfs(d, &bf);
-	temp = 0;
-	// while (temp < 53)
-	// {
-	// 	bfs(d, &bf);
-	// 	temp++;
-	// }
-	while (bfs(d, &bf))
-		temp++;
-	// double_check_paths(d, bf);
-	// printf("\nSECOND BFS:\n");
-	// size_t	i = 0;
-	// size_t	j = 0;
-	// t_room 		*room;
-	// while (bf.best->paths[i][j] != -1)
-	// {
-	// 	while (bf.best->paths[i][j] != -1)
-	// 	{
-	// 		room = ft_vecget(&d->rooms, bf.best->paths[i][j]);
-	// 		printf("|%s| ", room->name);
-	// 		j++;
-	// 		// if (bf.best->paths[i][j] != -1)
-	// 			// printf("-> ");
-	// 	}
-	// 	j = 0;
-	// 	i++;
-	// 	// printf("\n-------------------\n");
-	// }
-	// debug_bfs(d, bf, set);
-	printf(">>>> %d <<<<\n", bf.best->turns);
-	// double_check_paths(d, &bf);
-	// printf("BFS RETRY:\n>>>> %d <<<<\n", bf.best->turns);
-	// printf(">>>> %d <<<<\n", bf->best->paths[2][0]);
-	if (!bf.best)
-		return (-1);
+	init_bfs(d, bf);
+	ret = 1;
+	while (ret == 1)
+	{
+		ret = bfs(d, bf);
+		if (ret < 0)
+			return (panic(NULL, "Error: Memory issue in bfs."));
+	}
+	if (!bf->best)
+		return (panic(NULL, "Error: No solution."));
+	
 	return (1);
 }
