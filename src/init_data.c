4d96f57879a54f1dc639679a4e67fbb121f81d78
Antti-pekka Tenhunen <atenhune@c2r2p2.hive.fi>
2022-10-03 20:32:14 +0300

    added init_bfs() and zero_bfs()

--- src/init_data.c
+++ src/init_data.c
@@ -3,14 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   init_data.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 15:56:51 by altikka           #+#    #+#             */
-/*   Updated: 2022/09/30 22:49:17 by altikka          ###   ########.fr       */
+/*   Updated: 2022/10/03 17:08:22 by atenhune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
+
+static void	zero_bfs(t_lem *d, t_bfs *bf)
+{
+	int	i;
+
+	ft_memset(bf->fl_dir, -1, sizeof(int) * d->room_count);
+	i = 0;
+	while (i < d->room_count)
+		ft_memset(bf->flow[i++], 0, sizeof(int) * d->room_count);
+}
+
+int	init_bfs(t_lem *d, t_bfs *bfs)
+{
+	int	i;
+
+	bfs->best = NULL;
+	bfs->fl_dir = malloc(sizeof(int) * d->room_count);
+	if (!bfs->fl_dir)
+		return (panic(NULL, "Error: Couldn't initialize flow directions."));
+	bfs->flow = (int **)malloc(sizeof(int *) * d->room_count);
+	if (!bfs->flow)
+		return (panic(NULL, "Error: Couldn't initialize flow chart."));
+	i = 0;
+	while (i < d->room_count)
+	{
+		bfs->flow[i] = malloc(sizeof(int) * d->room_count);
+		if (!bfs->flow[i])
+			return (panic(NULL, "Error: Couldn't initialize flow chart."));
+		i++;
+	}
+	zero_bfs(d, bfs);
+	return (1);
+}
 
 static void	init_hash_table(t_parser *p)
 {
