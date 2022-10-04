1fc48fc613eb95336aebbd7a44b4a13d2abf193b
home <aleksandr.tikka@gmail.com>
2022-10-04 23:37:21 +0300

    cleaned up main from most comments

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 11:07:37 by altikka           #+#    #+#             */
-/*   Updated: 2022/10/04 19:56:35 by altikka          ###   ########.fr       */
+/*   Updated: 2022/10/04 23:36:36 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,35 +21,6 @@
 ** iv: print
 */
 
-static void	test_print(t_lem *d)
-{
-	t_room	*room;
-	t_room	*link;
-	int		*n;
-	size_t	i;
-	size_t	j;
-
-	return ;
-	ft_printf("\n"); //whole function will be removed once print() is done
-	i = 0;
-	while (i < d->rooms.len)
-	{
-		room = ft_vecget(&d->rooms, i);
-		ft_printf("room: %s,\tindex: [%d],\tlinks: ", room->name, room->index);
-		j = 0;
-		while (j < room->links.len)
-		{
-			n = ft_vecget(&room->links, j);
-			link = ft_vecget(&d->rooms, *n);
-			ft_printf("%s ", link->name);
-			j++;
-		}
-		ft_printf("\n");
-		i++;
-	}
-	ft_printf("\n");
-}
-
 int	main(int argc, char **argv)
 {
 	t_lem	d;
@@ -63,15 +34,10 @@ int	main(int argc, char **argv)
 		return (panic(NULL, "Error: Initializing data failed."));
 	if (parse_data(&d, &farm) < 0)
 		return (panic(&d, "Error: Parsing data failed."));
-	//system("leaks -q lem-in");
-	test_print(&d); //debugging, remove later
 	if (solve(&d, &bf) < 0)
 		return (panic(&d, "Error: No solution."));
-	system("leaks -q lem-in");
-	// remember to free bfs
 	if (print(&d, &bf, &farm) < 0)
 		return (panic(&d, "Error: Failed to print."));
-	// ft_vecdel(&farm); //will be freed in print()
 	free_data(&d);
 	//system("leaks -q lem-in");
 	return (0);
