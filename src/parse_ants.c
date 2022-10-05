222744aa06309eaba9966da8db190a93d21ea71d
Aleksandr Tikka <altikka@student.hive.fi>
2022-10-05 19:58:01 +0300

    better error handling when ant number not given at all

--- src/parse_ants.c
+++ src/parse_ants.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 11:06:32 by altikka           #+#    #+#             */
-/*   Updated: 2022/09/30 07:44:23 by altikka          ###   ########.fr       */
+/*   Updated: 2022/10/05 19:51:49 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	parse_ants(t_lem *d, t_parser *p)
 {
 	if (!ft_isnbr(p->line))
-		return (-1);
+		return (panic(NULL, "Error: Has nothing to do with ants."));
 	d->ants = ft_atoi(p->line);
 	if (d->ants < 0)
 		return (panic(NULL, "Error: Negative ammount of ants."));
