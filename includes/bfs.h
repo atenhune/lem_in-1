6e58a00ea05d2c7c865f3d56454584546b50cb71
Aleksandr Tikka <altikka@student.hive.fi>
2022-10-05 19:38:12 +0300

    deleted those prototypes that became static in their respective .c files

--- includes/bfs.h
+++ includes/bfs.h
@@ -6,12 +6,18 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 10:51:41 by atenhune          #+#    #+#             */
-/*   Updated: 2022/10/05 14:47:01 by atenhune         ###   ########.fr       */
+/*   Updated: 2022/10/05 18:32:59 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BFS_H
 # define BFS_H
+
+typedef struct s_pos
+{
+	int	i;
+	int	j;
+}		t_pos;
 
 typedef struct s_bfs
 {
@@ -20,6 +26,7 @@ typedef struct s_bfs
 	t_pathset	*best;
 }				t_bfs;
 
+// what to do what to do
 void	debug_bfs(t_lem *d, t_bfs *bf, t_pathset *set);
 
 int		init_bfs(t_lem *d, t_bfs *bfs);
@@ -28,19 +35,12 @@ void	set_seen_from_start(t_lem *d, t_bfs *bf, t_pathset *set);
 int		is_next(t_bfs *bf, t_pathset *set);
 int		stepped_on_a_path(t_bfs *bf, t_pathset *set);
 void	update_seen(t_lem *d, t_bfs *bf, t_pathset *set);
-void	reverse_flow(t_lem *d, t_bfs *bf, t_pathset *set);
-void	place_flow(t_bfs *bf, int cur, int next);
-int		is_child(int *list, int prev);
 void	set_flow(t_lem *d, t_bfs *bf, t_pathset *set);
 void	path_collector(t_lem *d, t_bfs *bf, t_pathset *set);
-void	set_fl_dir(t_lem *d, t_bfs *bf, t_pathset *set);
-
+void	set_flow_dir(t_lem *d, t_bfs *bf, t_pathset *set);
 int		turn_amount(t_lem *d, t_pathset *set);
-void	place_ant(t_pathset *set);
-int		last_ant(t_pathset *set);
 
 // bfs retry
-
 void	double_check_paths(t_lem *d, t_bfs *bf);
 
 #endif
