33d1f43fe1253fdb900a4bcdc4daedeb1e9c72f6
Aleksandr Tikka <altikka@student.hive.fi>
2022-10-05 19:45:29 +0300

    do nothing if start links to the end

--- src/solve.c
+++ src/solve.c
@@ -6,25 +6,20 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 10:55:18 by atenhune          #+#    #+#             */
-/*   Updated: 2022/10/03 15:03:57 by atenhune         ###   ########.fr       */
+/*   Updated: 2022/10/05 19:31:57 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-// static void	sort_paths(t_lem *d, t_bfs *bf)
-// {
-// 	size_t	i;
-
-// 	i = 0;
-	
-// }
-
 int	solve(t_lem *d, t_bfs *bf)
 {
 	int	ret;
 
-	init_bfs(d, bf);
+	if (d->start_to_end == true)
+		return (1);
+	if (init_bfs(d, bf) < 0)
+		return (panic(NULL, "Error: Failed to initialize bfs."));
 	ret = 1;
 	while (ret == 1)
 	{
@@ -34,6 +29,5 @@ int	solve(t_lem *d, t_bfs *bf)
 	}
 	if (!bf->best)
 		return (panic(NULL, "Error: No solution."));
-	
 	return (1);
 }
