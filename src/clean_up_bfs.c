9e04fca2e1c0fa4f56b18769112a2339363a0813
home <aleksandr.tikka@gmail.com>
2022-10-06 19:46:24 +0300

    quick norm

--- src/clean_up_bfs.c
+++ src/clean_up_bfs.c
@@ -6,13 +6,13 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 08:57:39 by altikka           #+#    #+#             */
-/*   Updated: 2022/10/06 09:03:10 by altikka          ###   ########.fr       */
+/*   Updated: 2022/10/06 19:43:53 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-void free_bfs(t_lem *d, t_bfs *bf)
+void	free_bfs(t_lem *d, t_bfs *bf)
 {
 	free_intarr((void *)bf->flow, d->room_count);
 	free(bf->fl_dir);
