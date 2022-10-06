648c1ec095bfe75f122bba69b934f2c334ef7c2d
home <aleksandr.tikka@gmail.com>
2022-10-06 19:54:57 +0300

    moved all parser structs and functions to parse.h

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -6,7 +6,7 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 11:09:22 by altikka           #+#    #+#             */
-/*   Updated: 2022/10/06 07:58:58 by altikka          ###   ########.fr       */
+/*   Updated: 2022/10/06 19:54:01 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include "libft.h"
 # include "hashmap.h"
 # include "data.h"
+# include "parse.h"
 # include "pathset.h"
 # include "bfs.h"
 # include "print.h"
@@ -26,7 +27,7 @@
 //////////////////////
 
 /*
-**STRUCTIONS FOR THE PROGRAM
+**STRUCTION FOR THE PROGRAM
 */
 
 typedef struct s_room
@@ -39,45 +40,6 @@ typedef struct s_room
 }			t_room;
 
 /*
-**STRUCTIONS & FUNCTIONS FOR THE PARSER
-*/
-
-typedef enum e_state
-{
-	ANTS,
-	ROOMS,
-	LINKS
-}			t_state;
-
-typedef struct s_parser
-{
-	t_vec	inputs;
-	char	*line;
-	int		start;
-	int		end;
-	t_hash	table[TABLE_SIZE];
-	t_state	state;
-}			t_parser;
-
-int		parse_ants(t_lem *d, t_parser *p);
-int		parse_rooms(t_lem *d, t_parser *p);
-int		parse_links(t_lem *d, t_parser *p);
-
-typedef int				(*t_parsers)(t_lem *, t_parser *);
-
-static const t_parsers	g_parsers[3] = {
-	parse_ants,
-	parse_rooms,
-	parse_links
-};
-
-int		init_parser(t_parser *p);
-int		return_next_line(const int fd, char **line, int *ret);
-int		panic_parser(t_parser *p, const char *msg);
-void	free_parser(t_parser *p);
-int		prepare_data(t_lem *d);
-
-/*
 **FUNCTIONS FOR THE PROGRAM
 */
 
@@ -87,7 +49,5 @@ int		parse_data(t_lem *d, t_vec *farm);
 void	free_data(t_lem *d);
 int		solve(t_lem *d, t_bfs *bf);
 int		print(t_lem *d, t_bfs *bf, t_vec *farm);
-
-void	free_intarr(void **apa, size_t n);
 
 #endif
