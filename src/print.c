837a22748bf56ba16c7c1bfe09f11c7a187c058f
home <aleksandr.tikka@gmail.com>
2022-10-07 11:33:55 +0300

    overhauled
    moved most print related function calls to print_options

--- src/print.c
+++ src/print.c
@@ -6,24 +6,43 @@
 /*   By: antti <antti@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 12:16:08 by atenhune          #+#    #+#             */
-/*   Updated: 2022/10/06 19:45:38 by altikka          ###   ########.fr       */
+/*   Updated: 2022/10/07 11:05:12 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
+static int	print_send_all(t_lem *d, t_vec *farm)
+{
+	t_room	*temp;
+	int		ant_nbr;
+
+	if (d->flag != QUIET)
+		write(1, farm->data, farm->len);
+	ft_vecdel(farm);
+	if (d->flag == QUIET)
+		return (ft_putendl(">>>> 1 <<<<"), 1);
+	//if (d->flag == PATHS)
+		//prints right stuff in right format
+	ant_nbr = 1;
+	temp = ft_vecget(&d->rooms, d->end);
+	while (ant_nbr <= d->ants)
+	{
+		ft_printf("L%d-%s", ant_nbr, temp->name);
+		ant_nbr++;
+		if (ant_nbr <= d->ants)
+			ft_printf(" ");
+	}
+	ft_printf("\n");
+	return (1);
+}
+
 int	print(t_lem *d, t_bfs *bf, t_vec *farm)
 {
 	t_printer	p;
 
-	write(1, farm->data, farm->len);
-	ft_vecdel(farm);
 	if (d->start_to_end == true)
-	{
-		if (print_send_all(d) < 0)
-			return (panic(NULL, "Error: Couldn't send all ants at once."));
-		return (1);
-	}
+		return (print_send_all(d, farm));
 	sort_paths(bf);
 	if (init_printer(bf, &p) < 0)
 	{
@@ -36,53 +55,8 @@ int	print(t_lem *d, t_bfs *bf, t_vec *farm)
 		free_bfs(d, bf);
 		return (panic_printer(&p, "Error: Couldn't send ants."));
 	}
-	write(1, p.result.data, p.result.len);
+	print_options(d, bf, &p, farm);
 	free_printer(&p);
-	ft_printf("\n>>>> %d <<<<\n", bf->best->turns);
 	free_bfs(d, bf);
-
-	// exit(0);
-	// int temp = 0;
-	// int temp2 = 0;
-	// while(temp < 3)
-	// {
-	// 	while(temp2 < 3)
-	// 		printf("%d ", p.ants_on_paths[temp][temp2++]);
-	// 	printf("\n");
-	// 	temp2 = 0;
-	// 	temp++;
-	// }
-	// printf("\n");
-	// ft_printf(">>>> %d <<<<\n", bf->best->turns);
-
-	// int a = 0;
-	// printf("ANT_LINE:\n");
-	// while (a < bf->best->count)
-	// 	printf("%2d ", p.ant_line[a++]);
-	// a = 0;
-	// printf("\nWEIGHT:\n");
-	// while (a < bf->best->count)
-	// 	printf("%2d ", bf->best->weight[a++]);
-	// // printf("\n weight: %d\n", bf->best->weight[9]);
-	// printf("\n PATH_COUNT: %d\n", bf->best->count);
-	// size_t	i = 0;
-	// size_t	j = 0;
-	// t_room 		*room;
-	// while (bf->best->paths[i][j] != -1)
-	// {
-	// 	while (bf->best->paths[i][j] != -1)
-	// 	{
-	// 		room = ft_vecget(&d->rooms, bf->best->paths[i][j]);
-	// 		printf("|%s| ", room->name);
-	// 		j++;
-	// 		if (bf->best->paths[i][j] != -1)
-	// 			printf("-> ");
-	// 	}
-	// 	j = 0;
-	// 	i++;
-	// 	printf("\n-------------------\n");
-	// }
-	// exit(0);
-
 	return (1);
 }
