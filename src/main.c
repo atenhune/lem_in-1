33a222958e67db506b480c26af25fa98ea1e29a9
home <aleksandr.tikka@gmail.com>
2022-10-07 14:06:10 +0300

    added flag variable to be used in init_flag() and print()

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 11:07:37 by altikka           #+#    #+#             */
-/*   Updated: 2022/10/07 10:15:55 by altikka          ###   ########.fr       */
+/*   Updated: 2022/10/07 13:58:06 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,11 @@
 int	main(int argc, char **argv)
 {
 	t_lem	d;
+	t_flag	flag;
 	t_bfs	bf;
 	t_vec	farm;
 
-	if (init_flags(&d.flag, argc, argv) <= 0)
+	if (init_flags(&flag, argc, argv) <= 0)
 		return (ft_putstr(LEM_USAGE), 0);
 	if (init_data(&d, &farm) < 0)
 		return (panic(NULL, "Error: Initializing data failed."));
@@ -35,7 +36,7 @@ int	main(int argc, char **argv)
 		return (ft_vecdel(&farm), panic(&d, "Error: Parsing data failed."));
 	if (solve(&d, &bf) < 0)
 		return (ft_vecdel(&farm), panic(&d, "Error: No solution."));
-	if (print(&d, &bf, &farm) < 0)
+	if (print(&d, &bf, &farm, flag) < 0)
 		return (panic(&d, "Error: Failed to print."));
 	free_data(&d);
 	//system("leaks -q lem-in");
