d4c430ffc7fc01f22deac635712207ceed1ab3ab
home <aleksandr.tikka@gmail.com>
2022-10-07 16:16:28 +0300

    added new flag -t for testing with run.sh

--- includes/flags.h
+++ includes/flags.h
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 08:24:33 by altikka           #+#    #+#             */
-/*   Updated: 2022/10/07 14:27:30 by altikka          ###   ########.fr       */
+/*   Updated: 2022/10/07 16:14:15 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,21 @@
 # define BOLD_ON "\e[1m"
 # define BOLD_OFF "\e[m"
 
-# define LEM_USAGE "\nusage: ./lem-in [-hpq] < [file]\n\
+# define LEM_USAGE "\nusage: ./lem-in [-hpqt] < [file]\n\
  options:\n\
  	"BOLD_ON"-h"BOLD_OFF"\thelp\n\
-	"BOLD_ON"-p"BOLD_OFF"\tdisplay additional path information\n\
-	"BOLD_ON"-q"BOLD_OFF"\tquiet mode\n\n"
+	"BOLD_ON"-p"BOLD_OFF"\tadditional path information\n\
+	"BOLD_ON"-q"BOLD_OFF"\tquiet mode\n\
+	"BOLD_ON"-t"BOLD_OFF"\ttest mode\n\n"
 
-# define LEM_FLAGS "pq"
+# define LEM_FLAGS "pqt"
 
 typedef enum e_flag
 {
 		NONE,
 		PATHS,
 		QUIET,
+		TEST
 }		t_flag;
 
 int		init_flags(t_flag *flag, int argc, char **argv);
