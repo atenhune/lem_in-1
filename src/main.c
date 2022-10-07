84c08bcd8c81f26d27e84f4f2ee850272cef9e31
home <aleksandr.tikka@gmail.com>
2022-10-07 18:09:06 +0300

    removed leaks command

--- src/main.c
+++ src/main.c
@@ -6,15 +6,15 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 11:07:37 by altikka           #+#    #+#             */
-/*   Updated: 2022/10/07 13:58:06 by altikka          ###   ########.fr       */
+/*   Updated: 2022/10/07 18:08:13 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
 /* 
-** PLAN
-** +: some options like -q etc.
+** PROGRAM SEQUENCE
+** +: init flags for output
 ** i: init data
 ** ii: parse data
 ** iii: solve
@@ -25,8 +25,8 @@ int	main(int argc, char **argv)
 {
 	t_lem	d;
 	t_flag	flag;
-	t_bfs	bf;
 	t_vec	farm;
+	t_bfs	bf;
 
 	if (init_flags(&flag, argc, argv) <= 0)
 		return (ft_putstr(LEM_USAGE), 0);
@@ -39,6 +39,5 @@ int	main(int argc, char **argv)
 	if (print(&d, &bf, &farm, flag) < 0)
 		return (panic(&d, "Error: Failed to print."));
 	free_data(&d);
-	//system("leaks -q lem-in");
 	return (0);
 }
