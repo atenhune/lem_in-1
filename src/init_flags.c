7f6d9209251243ce3ec2c8ab814af5726dc430a3
home <aleksandr.tikka@gmail.com>
2022-10-07 11:28:09 +0300

    function that checks validity of argc & argv and sets flag accordingly
    BUG: doesn't set flag accordingly

---
+++ src/init_flags.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_flags.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/07 09:40:34 by altikka           #+#    #+#             */
+/*   Updated: 2022/10/07 11:13:21 by altikka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem_in.h"
+
+static int	set_flag(t_flag *flag, char *arg_ptr)
+{
+	char	*p;
+
+	p = ft_strchr(LEM_FLAGS, *arg_ptr++);
+	if (!p || *arg_ptr)
+		return (-1);
+	if (*arg_ptr == 'p')
+		*flag = PATHS;
+	if (*arg_ptr == 'q')
+		*flag = QUIET;
+	return (1);
+}
+
+int		init_flags(t_flag *flag, int argc, char **argv)
+{
+	char	*p;
+
+	*flag = NONE;
+	if (argc == 1)
+		return (1);
+	if (argc > 2)
+		return (panic(NULL, "Error: Too many options."));
+	p = argv[1];
+	if (*p++ != '-')
+		return (panic(NULL, "Error: Invalid option."));
+	if (*p == 'h')
+		return (0);
+	if (set_flag(flag, p) < 0)
+		return (-1);
+	return (1);
+}
