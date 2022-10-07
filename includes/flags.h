e704d27671938ed3dec8f2e016724538146736d4
home <aleksandr.tikka@gmail.com>
2022-10-07 17:46:44 +0300

    final clean up & norm

--- includes/flags.h
+++ includes/flags.h
@@ -6,34 +6,30 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 08:24:33 by altikka           #+#    #+#             */
-/*   Updated: 2022/10/07 16:14:15 by altikka          ###   ########.fr       */
+/*   Updated: 2022/10/07 17:32:57 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FLAGS_H
-
 # define FLAGS_H
-
-# define BOLD_ON "\e[1m"
-# define BOLD_OFF "\e[m"
 
 # define LEM_USAGE "\nusage: ./lem-in [-hpqt] < [file]\n\
  options:\n\
- 	"BOLD_ON"-h"BOLD_OFF"\thelp\n\
-	"BOLD_ON"-p"BOLD_OFF"\tadditional path information\n\
-	"BOLD_ON"-q"BOLD_OFF"\tquiet mode\n\
-	"BOLD_ON"-t"BOLD_OFF"\ttest mode\n\n"
+ \t\e[1m-h\e[m\thelp\n\
+ \t\e[1m-p\e[m\tadditional path information\n\
+ \t\e[1m-q\e[m\tquiet mode\n\
+ \t\e[1m-t\e[m\ttest mode\n\n"
 
 # define LEM_FLAGS "pqt"
 
 typedef enum e_flag
 {
-		NONE,
-		PATHS,
-		QUIET,
-		TEST
-}		t_flag;
+	NONE,
+	PATHS,
+	QUIET,
+	TEST
+}	t_flag;
 
-int		init_flags(t_flag *flag, int argc, char **argv);
+int	init_flags(t_flag *flag, int argc, char **argv);
 
 #endif
