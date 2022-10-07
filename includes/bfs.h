01fb9be21d687619f581825b0b47265f5fbff69f
home <aleksandr.tikka@gmail.com>
2022-10-07 17:46:28 +0300

    final clean up & norm

--- includes/bfs.h
+++ includes/bfs.h
@@ -6,7 +6,7 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 10:51:41 by atenhune          #+#    #+#             */
-/*   Updated: 2022/10/06 09:10:37 by altikka          ###   ########.fr       */
+/*   Updated: 2022/10/07 17:34:25 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,6 @@ typedef struct s_bfs
 	t_pathset	*best;
 }				t_bfs;
 
-// what to do what to do
-void	debug_bfs(t_lem *d, t_bfs *bf, t_pathset *set);
-
 int		init_bfs(t_lem *d, t_bfs *bfs);
 int		bfs(t_lem *d, t_bfs *bf);
 int		panic_bfs(t_lem *d, t_bfs *bf, const char *msg);
@@ -41,8 +38,5 @@ void	set_flow(t_lem *d, t_bfs *bf, t_pathset *set);
 void	path_collector(t_lem *d, t_bfs *bf, t_pathset *set);
 void	set_flow_dir(t_lem *d, t_bfs *bf, t_pathset *set);
 int		turn_amount(t_lem *d, t_pathset *set);
-
-// bfs retry
-void	double_check_paths(t_lem *d, t_bfs *bf);
 
 #endif
