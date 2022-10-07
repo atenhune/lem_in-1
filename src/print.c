164d8504555bbe97a4708516ca206978caba7107
home <aleksandr.tikka@gmail.com>
2022-10-07 18:00:24 +0300

    moved print_send_all() to print_options.c

--- src/print.c
+++ src/print.c
@@ -6,39 +6,11 @@
 /*   By: atenhune <atenhune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 12:16:08 by atenhune          #+#    #+#             */
-/*   Updated: 2022/10/07 16:11:47 by altikka          ###   ########.fr       */
+/*   Updated: 2022/10/07 17:55:49 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
-
-static int	print_send_all(t_lem *d, t_vec *farm, t_flag flag)
-{
-	t_room	*temp;
-	int		ant_nbr;
-
-	if (flag != QUIET)
-		write(1, farm->data, farm->len);
-	ft_vecdel(farm);
-	if (flag == QUIET)
-		return (ft_putendl("Solved in 1 turn."), 1);
-	if (flag == TEST)
-		return (ft_putendl(">>>> 1 <<<<"), 1);
-	ft_printf("\n");
-	temp = ft_vecget(&d->rooms, d->end);
-	if (flag == PATHS)
-		ft_printf("Path:\n|%s|\n", temp->name);
-	ant_nbr = 1;
-	while (ant_nbr <= d->ants)
-	{
-		ft_printf("L%d-%s", ant_nbr, temp->name);
-		ant_nbr++;
-		if (ant_nbr <= d->ants)
-			ft_printf(" ");
-	}
-	ft_printf("\n");
-	return (1);
-}
 
 int	print(t_lem *d, t_bfs *bf, t_vec *farm, t_flag flag)
 {
